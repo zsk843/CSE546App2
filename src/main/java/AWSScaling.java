a9ece83a000fed818ceb12d5af9db66ea9fba5c3
zsk84 <szhan245@asu.edu>
2019-03-15 10:46:19 -0700

    initial

--- src/main/java/AWSScaling.java
+++ src/main/java/AWSScaling.java
@@ -1,103 +1,120 @@
 import com.amazonaws.services.ec2.model.*;
-import com.amazonaws.services.simpledb.model.GetAttributesRequest;
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.*;
 import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+
 
 public class AWSScaling{
     private static final String QUEUE_NAME = "bbQueue";
     private static final int MAX_NUM_INSTANCES = 5;
 
-    private List<String> appList = new ArrayList<String>();
-    private Map<String, Boolean> status = new HashMap<String, Boolean>();
-    private static String IMAGE_ID = "ami-0b30c5f01b371639f";
+    private static final String IMAGE_ID = "ami-0b30c5f01b371639f";
+    private AmazonEC2 ec2;
+
+    public AWSScaling(){
+        ec2 = AmazonEC2ClientBuilder.defaultClient();
+    }
 
     public void scaleApplication() {
 
-        final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
         int queueSize = getSQSLength();
         int requiredNum = Math.min(queueSize, MAX_NUM_INSTANCES);
 
         List<Instance> instances = this.getAllInstances();
         int runningNum = 0;
-        List<Instance> stoppedInstance = new LinkedList<Instance>();
+        LinkedList<Instance> stoppedInstance = new LinkedList<Instance>();
         for(Instance i: instances){
             String stateName = i.getState().getName().toLowerCase();
             if(stateName.equals( "running"))
                 runningNum ++;
             else if(stateName.equals("stopping")|| stateName.equals("stopped"))
-                instances.add(i);
+                stoppedInstance.add(i);
         }
 
+        if(runningNum < requiredNum){
+            int addNum = requiredNum - runningNum;
+            while(stoppedInstance.size()>0) {
+                Instance i = stoppedInstance.get(0);
+                stoppedInstance.removeFirst();
+                if(i.getState().getName().toLowerCase().equals("stopping")) {
+                    String status = "stopping";
+                    do {
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
+                        DescribeInstancesRequest desRequest = new DescribeInstancesRequest().withInstanceIds(i.getInstanceId());
+                        DescribeInstancesResult res = ec2.describeInstances(desRequest);
 
-        System.out.println("create an instance");
+                        status = res.getReservations().get(0).getInstances().get(0).getState().getName().toLowerCase();
+                    }while (status.equals("stopping"));
+                }
 
-          //image id of the instance
-        int minInstanceCount = 1; //create 1 instance
-        int maxInstanceCount = 1;
+                StartInstancesRequest request = new StartInstancesRequest().withInstanceIds(i.getInstanceId());
+                ec2.startInstances(request);
+                Log.Log("Restarting Instance");
+                addNum--;
+                if(addNum <= 0)
+                    break;
+            }
+            while(addNum > 0){
+                RunInstancesRequest rir = new RunInstancesRequest(IMAGE_ID,
+                        1, 1);
+                rir.setInstanceType("t2.micro");
+                ec2.runInstances(rir);
 
-        RunInstancesRequest rir = new RunInstancesRequest(imageId,
-                minInstanceCount, maxInstanceCount);
-        rir.setInstanceType("t2.micro"); //set instance type
-
-        RunInstancesResult result = ec2.runInstances(rir);
-
-        List<Instance> resultInstance =
-                result.getReservation().getInstances();
-
-        for(Instance ins : resultInstance) {
-            System.out.println("New instance has been created:" +
-                    ins.getInstanceId());//print the instance ID
-        }
-    }
-
-
-
-    public void createQueue() {
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-        try {
-            CreateQueueRequest createQueueRequest =
-                    new CreateQueueRequest(QUEUE_NAME);
-            String myQueueUrl = sqs.createQueue(createQueueRequest)
-                    .getQueueUrl();
-            System.out.println(myQueueUrl);
-        } catch (AmazonSQSException e) {
-            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
-                throw e;
+                addNum -- ;
             }
         }
+
     }
 
-    public void sendMsg() {
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
-        SendMessageRequest send_msg_request = new SendMessageRequest()
-                .withQueueUrl(queueUrl)
-                .withMessageBody("hello world");
-        sqs.sendMessage(send_msg_request);
-        System.out.println("send a msg");
-    }
+//    public void createQueue() {
+//        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+//        try {
+//            CreateQueueRequest createQueueRequest =
+//                    new CreateQueueRequest(QUEUE_NAME);
+//            String myQueueUrl = sqs.createQueue(createQueueRequest)
+//                    .getQueueUrl();
+//            System.out.println(myQueueUrl);
+//        } catch (AmazonSQSException e) {
+//            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+//                throw e;
+//            }
+//        }
+//    }
 
-    public void receiveMsg() {
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+//    public void sendMsg() {
+//        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+//        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+//        SendMessageRequest send_msg_request = new SendMessageRequest()
+//                .withQueueUrl(queueUrl)
+//                .withMessageBody("hello world");
+//        sqs.sendMessage(send_msg_request);
+//        System.out.println("send a msg");
+//    }
 
-        List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
-        for (Message m : messages) {
-            System.out.println(m.getBody());
-            sqs.deleteMessage(queueUrl, m.getReceiptHandle());
-        }
-
-        System.out.println("deleted msgs");
-    }
+//    public void receiveMsg() {
+//        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+//        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+//
+//        List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
+//        for (Message m : messages) {
+//            System.out.println(m.getBody());
+//            sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+//        }
+//
+//        System.out.println("deleted msgs");
+//    }
 
     public int getSQSLength(){
         final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
@@ -125,6 +142,9 @@ public class AWSScaling{
             request.setNextToken(response.getNextToken());
             if (response.getNextToken() == null)
                 break;
+            for(Instance i:instances){
+                System.out.println(i.getTags());
+            }
 
         }
 
@@ -136,10 +156,7 @@ public class AWSScaling{
     public static void main(String[] args)
     {
         AWSScaling sqsExample = new AWSScaling();
-        //sqsExample.createQueue();
-//       sqsExample.sendMsg();
-//       sqsExample.sendMsg();
-        sqsExample.getInstancesDes();
+        sqsExample.scaleApplication();
 
 
        
---
+++ src/main/java/Log.java
@@ -0,0 +1,7 @@
+
+public class Log{
+
+    public static void Log(String s){
+        System.out.println(s);
+    }
+}
