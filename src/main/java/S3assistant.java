1a83070a5c7d100177d69b3c4f2005600eac6835
zsk843 <zsk843@gmail.com>
2019-03-15 17:35:59 -0700

    Revert "initial 2"
    This reverts commit 8b942e47

---
+++ src/main/java/S3assistant.java
@@ -0,0 +1,67 @@
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
+
+import java.io.File;
+
+
+
+public class S3assistant {
+    private final static AmazonS3 s3 = AmazonS3ClientBuilder
+            .standard().withRegion(Regions.US_WEST_1)
+            .build();
+    private final static String bucket = "darknetbucket";
+
+    //upload file
+    public void upload(String key, File file){
+        try {
+            s3.putObject(bucket, key, file);
+            System.out.println("File uploaded!");
+        }catch (AmazonServiceException e){
+            System.err.println(e.getErrorMessage());
+        }
+    }
+
+    //upload content
+    public void upload(String key, String content){
+        try {
+            s3.putObject(bucket, key, content);
+            System.out.println("Content uploaded!");
+        }catch (AmazonServiceException e){
+            System.err.println(e.getErrorMessage());
+        }
+    }
+
+    public void delete(String key){
+        try{
+            s3.deleteObject(bucket, key);
+            System.out.println(key + " Deleted!");
+        }catch(AmazonServiceException e){
+            System.err.println(e.getErrorMessage());
+        }
+    }
+
+
+    public static void main(String args[]){
+        S3assistant obj = new S3assistant();
+        //obj.upload("firstkey", "HelloWorld");
+        /*try{
+            File file = new File("helloworld");
+            System.out.println(file.getAbsolutePath());
+            obj.upload("secondkey", file);
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
+        //obj.upload("secondkey", file);
+        //System.out.println(file.getAbsolutePath());
+        //obj.delete("firstkey");
+        //obj.delete("secondkey");
+
+    }
+}
