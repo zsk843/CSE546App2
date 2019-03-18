d8f1dad6b49a3e6439c40514eace204c139913e9
zsk84 <szhan245@asu.edu>
2019-03-17 19:48:37 -0700

    Shell script changed

--- start_app.sh
+++ start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar /ubuntu/home/app_tier.jar > /ubuntu/home/app_tier.log &
-echo $! > /ubuntu/home/app_tier.pid
+java -jar /home/ubuntu/app_tier.jar > /ubuntu/home/app_tier.log &
+echo $! > /home/ubuntu/app_tier.pid
