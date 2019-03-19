8f4a9aa118b7af05d4b5493ef5bcbd07ce6f2c39
zsk84 <szhan245@asu.edu>
2019-03-18 23:16:33 -0700

    Shell script changed

--- src/main/java/AWSScaling.java
+++ src/main/java/AWSScaling.java
@@ -21,7 +21,7 @@ public class AWSScaling {
     private static final String QUEUE_NAME = "request-queue";
     private static final int MAX_NUM_INSTANCES = 5;
     private static final String INSTANCE_NAME_PRE = "app-instance";
-    private static final String IMAGE_ID = "ami-0d6285f03741fbf35";
+    private static final String IMAGE_ID = "ami-05acbf47bb3bf5899";
     private static final int QUEUE_TIME_OUT_SEC = 5;
 
     // AWS Service Clients
